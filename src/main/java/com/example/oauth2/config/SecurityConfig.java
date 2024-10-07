04311189b1fd7fa3337b42bef3c5e190a7762385
owner <toltolyibh@gmail.com>
2024-10-07 18:54:29 +0900

    무한 로그인 해결

--- src/main/java/com/example/oauth2/config/SecurityConfig.java
+++ src/main/java/com/example/oauth2/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -55,7 +56,7 @@ public class SecurityConfig {
         //HTTP Basic 인증 방식 disable
         http.httpBasic((auth) -> auth.disable());
 
-        http.addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
+        http.addFilterAfter(new JWTFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
 
         //oauth2
         http.oauth2Login((oauth2) -> oauth2.userInfoEndpoint((userInfoEndpointConfig -> userInfoEndpointConfig.userService(customOAuth2UserService)))
